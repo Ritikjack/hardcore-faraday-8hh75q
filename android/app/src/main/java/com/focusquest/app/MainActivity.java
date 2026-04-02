7e815a4db67ece28c4fe822599b91fa20eacf138
Ritik <your-mrj19794@gmail.com>
2026-04-02 15:12:36 +0530

    first commit

---
+++ android/app/src/main/java/com/focusquest/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.focusquest.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.focusquest.app',
+  appName: 'FocusQuest',
+  webDir: 'build'
+};
+
+export default config;
